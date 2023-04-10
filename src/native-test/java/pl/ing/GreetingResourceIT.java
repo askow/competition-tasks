5840a07eabfa4f5e3c69c91068a794728a3b55c5
askow <askowina@gmail.com>
2023-04-10 19:28:26 +0200

    feat: base Quarkus template from https://code.quarkus.io

---
+++ src/native-test/java/pl/ing/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package pl.ing;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
